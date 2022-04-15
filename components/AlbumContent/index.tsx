1ea083836a07a2a3c69cbfb70c6c3f42b1392024
이수동 <tnehd1998@gmail.com>
2022-04-15 11:10:21 +0900

    refactor: AlbumContent의 메뉴 버튼 AlbumMenu 컴포넌트로 리팩토링

--- components/AlbumContent/index.tsx
+++ components/AlbumContent/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { TopSong } from "../../shared/type";
+import AlbumMenu from "../AlbumMenu";
 import {
   Artist,
   Button,
@@ -23,40 +24,7 @@ const AlbumContent = ({ album, rank }: IProps) => {
         <Name>{album.name}</Name>
         <Artist>{album.artists[0].name}</Artist>
       </Description>
-      <ButtonWrapper>
-        <Button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 4v16m8-8H4"
-            />
-          </svg>
-        </Button>
-        <Button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-            />
-          </svg>
-        </Button>
-      </ButtonWrapper>
+      <AlbumMenu />
     </Wrapper>
   );
 };
---
+++ components/AlbumMenu/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button, Wrapper } from "./styles";
+
+const AlbumMenu = () => {
+  return (
+    <Wrapper>
+      <Button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 4v16m8-8H4"
+          />
+        </svg>
+      </Button>
+      <Button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+          />
+        </svg>
+      </Button>
+    </Wrapper>
+  );
+};
+
+export default AlbumMenu;
