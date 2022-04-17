f032a5eec5179c2fa7512b56d3b0bd9363ba63a5
이수동 <tnehd1998@gmail.com>
2022-04-17 12:14:39 +0900

    fix: swr 제거 및 react-query로 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,21 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styled from "styled-components";
 import { getTopAlbums } from "../lib/spotify";
-import useSWR from "swr";
+import { useQuery } from "react-query";
 import SideMenu from "../components/SideMenu";
 import Chart from "../components/Chart";
+import { Suspense } from "react";
 
 const Home: NextPage = () => {
-  const { data: topAlbums, error } = useSWR("/api/songs", getTopAlbums);
+  const { data: topAlbums, error } = useQuery("topAlbums", getTopAlbums);
 
   if (error) {
     return <div>Error happened...</div>;
   }
 
-  if (!topAlbums) {
-    return <div>Loading...</div>;
-  }
+  // if (!topAlbums) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
     <Wrapper>
@@ -25,7 +26,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <SideMenu />
-      <Chart topAlbums={topAlbums} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Chart topAlbums={topAlbums} />
+      </Suspense>
     </Wrapper>
   );
 };
